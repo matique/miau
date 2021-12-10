729338eddfc88700e83c7703482708f5bd8adedc
dittmar <dittmar.krall@matique.de>
2021-12-10 10:55:24 +0100

    'v0.1.2'

--- lib/miau/version.rb
+++ lib/miau/version.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 module Miau
-  VERSION = "0.1.1" # 2021-11-06
+  VERSION = "0.1.2" # 2021-12-10
+  # VERSION = "0.1.1" # 2021-11-06
   # VERSION = "0.1.0" # 2021-09-13
   # VERSION = "0.0.2" # 2021-09-11
   # VERSION = "0.0.1"
