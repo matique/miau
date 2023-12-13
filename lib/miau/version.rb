0e3dbe2de8476bd157daeea354314f98f0fefee5
dittmar <dittmar.krall@matique.de>
2023-12-13 15:54:36 +0100

    'v1.0.3'

--- lib/miau/version.rb
+++ lib/miau/version.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 module Miau
-  VERSION = "1.0.2" # 2023-11-05
+  VERSION = "1.0.3" # 2023-12-13
+  # VERSION = "1.0.2" # 2023-11-05
   # VERSION = "1.0.1" # 2023-04-24
   # VERSION = "1.0.0" # 2022-01-23
   # VERSION = "0.1.2" # 2021-12-10
