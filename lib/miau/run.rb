986c95a3bd787e5ebc6d218b7f6272aa93647bbc
dittmar <dittmar.krall@matique.de>
2024-01-05 14:48:21 +0100

    '...'

--- lib/miau/run.rb
+++ lib/miau/run.rb
@@ -15,46 +15,21 @@ module Miau
     #   - method of ApplicationPolicy (independent of klass)
     #   - method of ApplicationPolicy specified by "miau action, method"
     #   - nil
-    # returns [klass, method]
-
-#    def find_policy(kls, action)
-#      name = "#{kls.to_s.camelcase}Policy"
-#      if Object.const_defined?(name)
-#        klass = name.constantize.new
-#        return action if klass.respond_to?(action)
-#
-#        hsh = PolicyStorage.instance.policies[kls]
-#        if hsh
-#          meth = hsh[action]
-#          return meth if meth
-#        end
-#        hsh = PolicyStorage.instance.policies[:application]
-#        if hsh
-#          meth = hsh[action]
-#          return meth if meth
-#        end
-#      end
-#
-#      nil
-#    end
+    # returns method_name[s]
 
     def find_policy(policy, klass, action)
-#ic policy, klass, action
       return action if policy.respond_to?(action)
 
       hsh = PolicyStorage.instance.policies[klass]
-#ic hsh
       return nil unless hsh
 
       hsh[action]
     end
 
     def run(klass, action, user, resource)
-#ic :run, klass, action, user, resource
       policy = PolicyStorage.instance.find_or_create_policy(klass)
       meth = find_policy policy, klass, action if policy
       meth ||= find_policy ApplicationPolicy, :application, action
-#ic meth
 
       unless meth
         msg = "class <#{klass}> action <#{action}>"
